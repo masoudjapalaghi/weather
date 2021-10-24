01a6fdb713a0ed7d7bb2e9d03a2acc7308e90101
masoud teymuri <masoudteymuri74@gmail.com>
2021-10-24 21:10:27 +0330

    fixed bug and handler  request http and add sweetalert2

--- src/container/weather.jsx
+++ src/container/weather.jsx
@@ -1,32 +1,55 @@
-import { isEmpty } from "lodash";
+import { isEmpty, isUndefined } from "lodash";
 import React, { Fragment, useEffect, useState } from "react";
-import { trackPromise } from 'react-promise-tracker';
+import { trackPromise } from "react-promise-tracker";
 import { getWeather } from "../services/weatherService";
 import { getLocalTime } from "../services/localTimeService";
 import Loading from "../utils/loading";
 import FormLocation from "../components/formLocation/formLocation";
+import Swal from "sweetalert2";
 
 function Weather() {
-  const [place, setPlace] = useState('');
+  const [place, setPlace] = useState("");
   const [localTime, setLocalTime] = useState();
+  const [network, setNetwork] = useState('online');
   const [weatherPlace, setWeatherPlace] = useState();
-  const [isLoading, setLoading] = useState(true)
+  const [isLoading, setLoading] = useState(true);
+
   const handleGetLocalTime = async () => {
-    try {
-      const timeLocation = await getLocalTime(place);
-      setLocalTime(timeLocation.data);
-    } catch (error) {
-      console.log(error);
+    if (network === "online") {
+      try {
+        const { data } = await getLocalTime(place);
+        setLocalTime(data);
+      } catch (error) {
+        console.log(error);
+        if(error){
+          setNetwork('offline')
+        }
+      }
+    } else {
+      Swal.fire({
+        icon: "error",
+        title: "Oops...",
+        text: "شما به شبکه متصل نیستید؟!؟",
+        footer: "<small>.اتصال اینترنت خود را چک کنید</small>",
+      });
     }
   };
   const handleGetWeather = async (city) => {
-    const { data,status } = await getWeather(city || place);
-    try {
-      setWeatherPlace(data);
-    } catch (error) {
-      console.log(error);
+    if (network === "online") {
+      try {
+        const { data } = await getWeather(city || place);
+        setWeatherPlace(data);
+      } catch (error) {
+        console.log(error);
+      }
+    } else {
+      Swal.fire({
+        icon: "error",
+        title: "Oops...",
+        text: "شما به شبکه متصل نیستید؟!؟",
+        footer: "<small>.اتصال اینترنت خود را چک کنید</small>",
+      });
     }
-
   };
 
   const kelvinToCelsius = (k) => {
@@ -34,22 +57,40 @@ function Weather() {
   };
   useEffect(() => {
     handleGetLocalTime();
-  }, [])
+    window.addEventListener("offline", function (e) {
+      setNetwork("offline");
+    });
+    window.addEventListener("online", function (e) {
+      setNetwork("online");
+    });
+    // window.addEventListener("refresh", function (e) {
+    //   setNetwork("online");
+    // });
+  }, []);
   useEffect(() => {
     if (!isEmpty(localTime)) {
       handleGetWeather(localTime.location.city);
     }
   }, [localTime]);
+  console.log(network);
   console.log(localTime);
   console.log(weatherPlace);
 
   return (
     <Fragment>
       <div className="App_weather">
-      <div className="weather_cart">
-        {/* <h2>{kelvinToCelsius(weatherPlace.main.temp)}</h2> */}
-      </div>
-       <FormLocation handleGetWeather={handleGetWeather} handleGetLocalTime={handleGetLocalTime} Place={place} setPlace={setPlace}/>
+        <div className="weather_cart">
+          <div className="card shadow-inset border-light ">
+            <div className="card-body shadow-soft  border-light "></div>
+          </div>
+          {/* <h2>{kelvinToCelsius(weatherPlace.main.temp)}</h2> */}
+        </div>
+        <FormLocation
+          handleGetWeather={handleGetWeather}
+          handleGetLocalTime={handleGetLocalTime}
+          Place={place}
+          setPlace={setPlace}
+        />
       </div>
     </Fragment>
   );
--- src/services/httpService.js
+++ src/services/httpService.js
@@ -1,11 +1,29 @@
 import axios from "axios";
-
+import { isEmpty } from "lodash";
+import Swal from "sweetalert2";
 axios.interceptors.response.use(null, (error) => {
+  const notFound = error.response.status == 404;
   const expectedError =
-    error.response && error.response >= 400 && error.response < 500;
-  if (!expectedError) {
-    alert("مشکلی از سمت سرور به وجود آمد است.");
+    error.response.status >= 400 &&
+    error.response.status < 500 &&
+    !error.response.status == 404;
+  if (notFound) {
+    Swal.fire({
+      icon: "info",
+      title: "Oops...",
+      text: "شهر مورد نظر یافت نشد؟!؟",
+      footer: "<small>.برای نتایج بهتر از فینگلیش استفاده کنید</small>",
+    });
+  } else {
+    Swal.fire({
+      icon: "error",
+      title: "Oops...",
+      text: "مشکلی از سمت سرور به وجود آمده است؟!؟",
+      footer: "<smalll>چند لحظه بعد امتحان کنید؟!؟  </smalll>",
+    });
   }
+
+  // console.log(error.response)
   return Promise.reject(error);
 });
 
